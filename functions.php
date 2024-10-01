ecf54c17ede0e06e19da846a3a1f7a46dc82210a
silvinr16 <silvinurhidayah6@gmail.com>
2024-10-01 18:43:51 +0700

    functions.php

--- functions.php
+++ functions.php
@@ -1,21 +1,26 @@
 <?php
-
-// Menambah Task baru
+// Fungsi untuk menambah task baru
 function addTask($task) {
     if (!isset($_SESSION['tasks'])) {
         $_SESSION['tasks'] = [];
     }
+    $task['status'] = 'Belum Selesai'; // Status default saat menambah task
     $_SESSION['tasks'][] = $task;
 }
 
-// Mengupdate Task yang sudah ada
+// Fungsi untuk mengupdate task yang sudah ada
 function updateTask($id, $task) {
     $_SESSION['tasks'][$id] = $task;
 }
 
-// Menghapus Task berdasarkan ID
+// Fungsi untuk menandai task sebagai selesai
+function completeTask($id) {
+    $_SESSION['tasks'][$id]['status'] = 'Selesai';
+}
+
+// Fungsi untuk menghapus task
 function deleteTask($id) {
     unset($_SESSION['tasks'][$id]);
-    $_SESSION['tasks'] = array_values($_SESSION['tasks']); // Reindex array
+    $_SESSION['tasks'] = array_values($_SESSION['tasks']); // Reindex array setelah penghapusan
 }
 ?>
